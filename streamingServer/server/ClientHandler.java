c0cfb00d9cd12b971dd918486001642dee23f207
Eragon587410 <francesco.roccabruna@marconirovereto.it>
2025-11-25 12:02:00 +0100

    [streaming] inizio implementazione lato python collegamento a server streaming

--- streamingServer/server/ClientHandler.java
+++ streamingServer/server/ClientHandler.java
@@ -1,4 +1,7 @@
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -6,18 +9,37 @@ public class ClientHandler extends Thread {
 
     Socket socket;
     String film;
+    byte[] buffer;
 
     public ClientHandler(Socket socket){
         this.socket = socket;
+        buffer = new byte[4096];
     }
 
     @Override
     public void run(){
         try{
+            System.out.println("cleint");
             BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
+            BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream());
             Scanner in = new Scanner(bis);
             String message = in.nextLine();
             System.out.println(message);
+            film = message;
+            // File file = new File("film/"+film);
+            bos.write(message.getBytes(), 0, message.getBytes().length); 
+            bos.flush();
+            socket.shutdownOutput(); //  IMPORTANTE
+            socket.close(); 
+            //FileInputStream fis = new FileInputStream(file);
+            //int read;
+
+            //while ((read = fis.read(buffer)) != -1){
+            //    bos.write(buffer, 0, read);
+            //}
+            //bos.flush();
+            //fis.close();
+            
         } catch (Exception a){
             System.err.println(a);
         }
